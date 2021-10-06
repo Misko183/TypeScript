bc2a8128a1d6370c1d4fb9d4c36d6245dafea5db
Michal Sládeček <michal.sladecek89@gmail.com>
2021-10-06 22:51:39 +0200

    Snail

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,4 @@
-const number1 = 3;
-const number2 = 4;
-const number3 = 3;
-
-console.log(EqualityOfThreeValues(number1, number2, number3))
-
-function EqualityOfThreeValues(number1, number2, number3){
-            if(number1 == number2 && number2 == number3){
-                        return 3;
-            }
-            else if(number1 == number2 || number1 == number3 || number2 == number3){
-                        return 2;
-            }
-            else{
-                        return 0;
-            }
+function totalDistance (heightOfStep : number, lenghtOfStep : number, towerHeight : number) {
+    console.log ((towerHeight / heightOfStep) * (lenghtOfStep + heightOfStep));
 }
+totalDistance(0.3, 0.2, 25.0);
